7efc5f0ef5cde92f07db4841fd6c210973373308
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-07 21:30:21 +0700

    Add link to twitter post body

--- src/components/posts/view-post/TwitterPost.tsx
+++ src/components/posts/view-post/TwitterPost.tsx
@@ -1,19 +1,22 @@
-import { PostContent } from '@subsocial/api/types'
+import { PostData, SpaceStruct } from '@subsocial/api/types'
 import { createTwitterURL, parseTwitterTextToMarkdown } from '@subsocial/utils'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { HTMLProps } from 'react'
 import { BsTwitter } from 'react-icons/bs'
 import { DfMd } from 'src/components/utils/DfMd'
+import ViewPostLink from '../ViewPostLink'
 import { PostImage } from './helpers'
 import styles from './TwitterPost.module.sass'
 
 export type TwitterPostProps = Omit<HTMLProps<HTMLDivElement>, 'content'> & {
-  content: PostContent
+  post: PostData
+  space: SpaceStruct | undefined
 }
 
-export default function TwitterPost({ content, ...props }: TwitterPostProps) {
-  if (!content.tweet) return null
+export default function TwitterPost({ post, space, ...props }: TwitterPostProps) {
+  const { content } = post
+  if (!content?.tweet) return null
 
   const parsedBody = parseTwitterTextToMarkdown(content.body)
   const originalTweetUrl = createTwitterURL(content.tweet)
@@ -37,9 +40,17 @@ export default function TwitterPost({ content, ...props }: TwitterPostProps) {
           </a>
         </Link>
       </div>
-      <div className={styles.TwitterPostBody}>
+      <div className={styles.TwitterPostContent}>
         <PostImage className='CursorPointer my-2' content={content} />
-        <DfMd source={parsedBody} />
+        <div className={styles.TwitterPostBodyContainer}>
+          <ViewPostLink
+            post={post}
+            space={space}
+            title=' '
+            className={styles.TwitterPostBodyLink}
+          />
+          <DfMd className={styles.TwitterPostBody} source={parsedBody} />
+        </div>
       </div>
     </div>
   )
