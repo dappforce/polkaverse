7cc9ffe1785b98badc85907c4eed060add512167
ical10 <mochhusnir@gmail.com>
2023-03-07 23:08:26 +0700

    Add mnemonic phrase generation

--- src/components/auth/signIn/email/ShowMnemonicModalContent.tsx
+++ src/components/auth/signIn/email/ShowMnemonicModalContent.tsx
@@ -2,6 +2,7 @@ import { Button, Card, Checkbox, Divider } from 'antd'
 import { useState } from 'react'
 import { Copy } from 'src/components/urls/helpers'
 import CopyOutlinedIcon from 'src/components/utils/icons/CopyOutlined'
+import useMnemonicGenerate from 'src/components/utils/OffchainSigner/useMnemonicGenerate'
 import { StepsEnum } from '../../AuthContext'
 import styles from './SignInModalContent.module.sass'
 
@@ -10,15 +11,16 @@ type Props = {
 }
 
 const ShowMnemonicModalContent = ({ setCurrentStep }: Props) => {
-  const MNEMONIC = 'nurse type awful punch cat pool brain artefact entire sight adult silly'
+  //TODO: only shown once (during sign up flow)
+  const { mnemonic } = useMnemonicGenerate()
   const [isMnemonicSaved, setIsMnemonicSaved] = useState(false)
 
   return (
     <div className={styles.ConfirmationStepContent}>
       <Card className={styles.InnerCard}>
-        <div className={styles.MnemonicText}>{MNEMONIC}</div>
+        <div className={styles.MnemonicText}>{mnemonic}</div>
         <Divider type={'vertical'} className={styles.InnerDivider} />
-        <Copy text={MNEMONIC} message='Your mnemonic phrase copied'>
+        <Copy text={mnemonic} message='Your mnemonic phrase copied'>
           <div className={styles.CopyIcon}>
             <CopyOutlinedIcon />
           </div>
