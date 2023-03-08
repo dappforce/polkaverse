e56721a3528db2495bb5ec113ae493ea1f2e47b2
ical10 <mochhusnir@gmail.com>
2023-03-09 01:52:22 +0700

    Extract button components out of Signer

---
+++ src/components/onboarding/OnBoardingModal/steps/Signer/ButtonGroup.tsx
@@ -0,0 +1,66 @@
+import { Button } from 'antd'
+import clsx from 'clsx'
+import RememberMeButton from 'src/components/utils/OffchainSigner/RememberMeButton'
+
+type ButtonGroupProps = {
+  goToNextStep: (config?: { forceTerminateFlow?: boolean; isSkipped?: boolean }) => void
+  loadingProxy: boolean
+  setLoadingProxy: (loadingProxy: boolean) => void
+  saveAsDraft: () => void
+  offchainSigner: boolean
+  onSuccess: () => void
+  isPartial?: boolean
+}
+
+const ButtonGroup = ({
+  isPartial,
+  goToNextStep,
+  loadingProxy,
+  setLoadingProxy,
+  saveAsDraft,
+  offchainSigner,
+  onSuccess,
+}: ButtonGroupProps) => {
+  return (
+    <>
+      <div className={clsx('DfButtonGroupRoot mt-4')}>
+        <Button
+          className={clsx('w-100')}
+          size='large'
+          onClick={() => {
+            if (isPartial) goToNextStep({ forceTerminateFlow: true })
+            goToNextStep()
+          }}
+        >
+          No, I’m fine
+        </Button>
+        <RememberMeButton
+          className={clsx('w-100')}
+          type='primary'
+          size='large'
+          withSpinner={true}
+          loading={loadingProxy}
+          disabled={loadingProxy}
+          onClick={() => {
+            setLoadingProxy(true)
+
+            saveAsDraft()
+            if (offchainSigner) {
+              goToNextStep()
+            } else {
+              setLoadingProxy(false)
+            }
+          }}
+          onFailedAuth={() => setLoadingProxy(false)}
+          onSuccessAuth={() => {
+            setLoadingProxy(false)
+            onSuccess()
+            if (!isPartial) goToNextStep()
+          }}
+        />
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup
---
+++ src/components/onboarding/OnBoardingModal/steps/Signer/ContinueButton.tsx
@@ -0,0 +1,48 @@
+import { useMyAddress } from 'src/components/auth/MyAccountsContext'
+import { getProxyAddress } from 'src/components/utils/OffchainSigner/ExternalStorage'
+import TxButton from 'src/components/utils/TxButton'
+
+type ContinueButtonProps = {
+  goToNextStep: (config?: { forceTerminateFlow?: boolean; isSkipped?: boolean }) => void
+  loading: boolean
+  setLoading: (loading: boolean) => void
+  isPartial?: boolean
+}
+
+const ContinueButton = ({ isPartial, loading, setLoading, goToNextStep }: ContinueButtonProps) => {
+  const myAddress = useMyAddress()
+
+  const proxyAddress = getProxyAddress(myAddress!)
+
+  return (
+    <TxButton
+      isFreeTx
+      type='primary'
+      block
+      size='large'
+      className='mt-4'
+      params={[proxyAddress, 'Any', 0]}
+      tx='proxy.addProxy'
+      onSuccess={() => {
+        setLoading(false)
+        if (isPartial) {
+          goToNextStep({ forceTerminateFlow: true })
+          return
+        }
+      }}
+      onFailed={() => setLoading(false)}
+      loading={loading}
+      disabled={loading}
+      onValidate={async () => {
+        return true
+      }}
+      onClick={async () => {
+        setLoading(true)
+      }}
+    >
+      Continue
+    </TxButton>
+  )
+}
+
+export default ContinueButton
