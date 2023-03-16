6f2cb3d59cf1d0bf1ef7f147219b4c8f27a02a70
ical10 <mochhusnir@gmail.com>
2023-03-16 18:14:24 +0700

    Move api calls to api file

--- src/components/utils/OffchainSigner/api/requests.ts
+++ src/components/utils/OffchainSigner/api/requests.ts
@@ -1,16 +1,47 @@
 import { isFunction } from '@polkadot/util'
 import { OffchainSignerEndpoint, sendHttpRequest, SendHttpRequestProps } from './utils'
 
-export const requestMessage = async (
-  myAddress: string,
+type EmailSignUpProps = {
+  email: string
+  password: string
+  accountAddress: string
+  signedProof: string
+  proof: string
+  hcaptchaResponse: string
+}
+
+type EmailSignInProps = {
+  email: string
+  password: string
+  hcaptchaResponse: string
+}
+
+type AddressSignInProps = {
+  signedProof: string
+  proof: string
+  hcaptchaResponse: string
+}
+
+type ConfirmEmailProps = {
+  code: string
+  accessToken: string
+}
+
+export type JwtPayload = {
+  accountAddress: string
+  accountAddressVerified: boolean
+  email: string
+  emailVerified: boolean
+  iat: number
+  exp: number
+}
+
+export const requestProof = async (
+  accountAddress: string,
   onFailedCallback?: (err: Error) => void,
 ) => {
-  if (!myAddress) {
-    throw new Error('No account id provided')
-  }
-
   const data = {
-    accountAddress: myAddress,
+    accountAddress,
   }
 
   const res = await sendHttpRequest({
@@ -27,40 +58,109 @@ export const requestMessage = async (
   return res
 }
 
-export const sendSignedMessage = async (
-  myAddress: string,
-  signedMessageJwt: string,
-  messageJwt: string,
-  token: string,
+export const addressSignIn = async (
+  props: AddressSignInProps,
   onFailedCallback: (err: Error) => void,
 ) => {
-  if (!token) throw new Error('Please confirm hCaptcha!')
-
-  const data = {
-    accountAddress: myAddress as string,
-    signedMessageJwt,
-    messageJwt,
-    hcaptchaResponse: token,
-  }
-
   const res = await sendHttpRequest({
     params: {
-      url: OffchainSignerEndpoint.SEND_SIGNED_PROOF,
-      data,
+      url: OffchainSignerEndpoint.ADDRESS_SIGN_IN,
+      data: props,
     },
     method: 'POST',
     onFaileReturnedValue:
-      isFunction(onFailedCallback) && onFailedCallback(new Error('Failed to send signed proof')),
-    onFailedText: 'Failed to send signed proof',
+      isFunction(onFailedCallback) && onFailedCallback(new Error('Failed to sign in with address')),
+    onFailedText: 'Failed to sign in with address',
   })
 
   return res
 }
 
-export const fetchProxyAddress = async (onFailedCallback?: (err: Error) => void) => {
+export const emailSignUp = async (
+  props: EmailSignUpProps,
+  onFailedCallback: (err: Error) => void,
+) => {
+  const res = await sendHttpRequest({
+    params: {
+      url: OffchainSignerEndpoint.SIGNUP,
+      data: props,
+    },
+    method: 'POST',
+    onFaileReturnedValue:
+      isFunction(onFailedCallback) && onFailedCallback(new Error('Failed to sign up with email')),
+    onFailedText: 'Failed to sign up with email',
+  })
+
+  return res
+}
+
+export const confirmEmail = async (props: ConfirmEmailProps) => {
+  const res = await sendHttpRequest({
+    params: {
+      url: OffchainSignerEndpoint.CONFIRM,
+      data: props.code,
+      config: {
+        headers: {
+          Authorization: props.accessToken,
+        },
+      },
+    },
+    method: 'POST',
+    onFaileReturnedValue: undefined,
+    onFailedText: 'Failed to confirm email',
+  })
+
+  return res
+}
+
+export const resendEmailConfirmation = async (accessToken: string) => {
+  const res = await sendHttpRequest({
+    params: {
+      url: OffchainSignerEndpoint.RESEND_CONFIRMATION,
+      config: {
+        headers: {
+          Authorization: accessToken,
+        },
+      },
+    },
+    method: 'POST',
+    onFaileReturnedValue: undefined,
+    onFailedText: 'Failed to resend email confirmation',
+  })
+
+  return res
+}
+
+export const emailSignIn = async (
+  props: EmailSignInProps,
+  onFailedCallback: (err: Error) => void,
+) => {
+  const res = await sendHttpRequest({
+    params: {
+      url: OffchainSignerEndpoint.SIGNIN,
+      data: props,
+    },
+    method: 'POST',
+    onFaileReturnedValue:
+      isFunction(onFailedCallback) && onFailedCallback(new Error('Failed to sign in with email')),
+    onFailedText: 'Failed to sign in with email',
+  })
+
+  return res
+}
+
+export const fetchMainProxyAddress = async (
+  accessToken: string,
+  onFailedCallback?: (err: Error) => void,
+) => {
   const res = await sendHttpRequest({
     params: {
       url: OffchainSignerEndpoint.FETCH_MAIN_PROXY,
+      config: {
+        headers: {
+          Authorization: accessToken,
+        },
+      },
     },
     method: 'GET',
     onFaileReturnedValue:
