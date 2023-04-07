86117297b28190e4e91916bba246ad9d36f2524b
ical10 <mochhusnir@gmail.com>
2023-04-07 21:34:45 +0700

    Add tooltip to NameChip

--- src/components/profiles/address-views/NameChip.tsx
+++ src/components/profiles/address-views/NameChip.tsx
@@ -1,6 +1,23 @@
+import { Tooltip } from 'antd'
 import clsx from 'clsx'
 import styles from './NameChip.module.sass'
 
 export default function NameChip({ ...props }) {
-  return <div {...props} className={clsx(styles.Chip)} />
+  return (
+    <div className='ml-auto'>
+      <Tooltip
+        className='ml-2'
+        placement='bottomRight'
+        title={
+          <>
+            <div>This account</div>
+            <div>is registered with</div>
+            <div>email</div>
+          </>
+        }
+      >
+        <div {...props} className={clsx(styles.Chip)} />
+      </Tooltip>
+    </div>
+  )
 }
