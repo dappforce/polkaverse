db229306ca2f4f8cdd154d3bb49d9c02e38785df
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-05-29 20:16:25 +0700

    Remove energy step if login with email

--- src/components/onboarding/hooks/useOnBoardingStepsOrder.ts
+++ src/components/onboarding/hooks/useOnBoardingStepsOrder.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { useAuth } from 'src/components/auth/AuthContext'
-import { useMyAddress } from 'src/components/auth/MyAccountsContext'
+import { useIsUsingEmail, useMyAddress } from 'src/components/auth/MyAccountsContext'
 import { isProxyAdded } from 'src/components/utils/OffchainSigner/ExternalStorage'
 import { useSelectProfile } from 'src/rtk/app/hooks'
 import { OnBoardingDataTypes } from 'src/rtk/features/onBoarding/onBoardingSlice'
@@ -20,6 +20,7 @@ export default function useOnBoardingStepsOrder(
   const profile = useSelectProfile(myAddress)
   const { isFollowSpaceModalUsed } = useIsFollowSpaceModalUsedContext()
   const { isProxyAdded: isProxyAddedState } = useIsProxyAddedContext()
+  const isUsingEmail = useIsUsingEmail()
 
   // hide sidebar button when accounts are already added with proxy
   const isCurrentAddressAddedWithProxy = isProxyAdded(myAddress!)
@@ -33,9 +34,10 @@ export default function useOnBoardingStepsOrder(
     if (!isCompact || !isFollowSpaceModalUsed) usedSteps.push('spaces')
     if (!profile?.content?.name) usedSteps.push('profile')
 
-    const showEnergyStep = !isCompact
-      ? (additionalData?.energySnapshot ?? 0) < 20
-      : transactionsCount < 20
+    const showEnergyStep =
+      !isUsingEmail && !isCompact
+        ? (additionalData?.energySnapshot ?? 0) < 20
+        : transactionsCount < 20
     if (showEnergyStep) usedSteps.push('energy')
 
     const showSignerStep = !isCurrentAddressAddedWithProxy && !isProxyAddedState
