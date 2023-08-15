0aa7b02be11bddad637788099bcb0e54a4aa8bd5
Mell <oleh.melnechyk@gmail.com>
2023-08-15 15:31:47 +0300

    Update staging config

--- src/config/connections/staging.ts
+++ src/config/connections/staging.ts
@@ -4,10 +4,10 @@ const stagingConfig: SubsocialConfig = {
   sudoOne: '3si9SFRhEPKvpk2xW9dUkcud1AanQxJwok2QFqppqE5ZeShp',
 
   substrateUrl: 'wss://rco-para.subsocial.network',
-  offchainUrl: 'https://staging-api.subsocial.network',
+  offchainUrl: 'https://api.subsocial.network',
   graphqlUrl: 'https://squid.subsquid.io/soonsocial/graphql',
 
-  ipfsNodeUrl: 'https://staging-ipfs.subsocial.network',
+  ipfsNodeUrl: 'https://ipfs.subsocial.network',
   dagHttpMethod: 'get',
   useOffchainForIpfs: false,
 
