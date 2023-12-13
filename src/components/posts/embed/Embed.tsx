f4b96e256ee5bca43a75615612400d98a611400c
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-12-13 16:42:02 +0700

    feat: use youtube lite embed

--- src/components/posts/embed/Embed.tsx
+++ src/components/posts/embed/Embed.tsx
@@ -1,11 +1,20 @@
 import clsx from 'clsx'
+import { useMemo } from 'react'
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import styles from './Embed.module.sass'
 
 type EmbedProps = {
   link: string
   className?: string
 }
 
-const allowEmbedList = ['vimeo', 'youtube', 'youtu.be', 'soundcloud']
+const allowEmbedList = ['vimeo', 'youtube', 'youtu.be', 'soundcloud'] as const
+const componentMap: {
+  [key in (typeof allowEmbedList)[number]]?: (props: { src: string }) => JSX.Element | null
+} = {
+  'youtu.be': YoutubeEmbed,
+  youtube: YoutubeEmbed,
+}
 
 const getEmbedUrl = (url: string, embed: string | undefined) => {
   if (!embed) return
@@ -25,6 +34,9 @@ const Embed = ({ link, className }: EmbedProps) => {
   const embed = allowEmbedList.find(embed => link.includes(embed))
   const src = getEmbedUrl(link, embed)
 
+  if (!src) return null
+  let Component = componentMap[embed as (typeof allowEmbedList)[number]] || GeneralEmbed
+
   return (
     <>
       {src && (
@@ -38,20 +50,59 @@ const Embed = ({ link, className }: EmbedProps) => {
             overflow: 'hidden',
           }}
         >
-          <iframe
-            width='100%'
-            height='100%'
-            src={src}
-            frameBorder='0'
-            loading='lazy'
-            allow='autoplay; encrypted-media; picture-in-picture;'
-            allowFullScreen
-            title='Embedded'
-            style={{ position: 'absolute', top: 0, left: 0 }}
-          />
+          <Component src={src} />
         </div>
       )}
     </>
+  )
+}
+
+function getYoutubeVideoId(youtubeLink: string) {
+  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+  const match = youtubeLink.match(regExp)
+  if (match && match[2].length == 11) {
+    return match[2]
+  } else {
+    return undefined
+  }
+}
+function YoutubeEmbed({ src }: { src: string }) {
+  const youtubeId = useMemo(() => getYoutubeVideoId(src), [src])
+
+  if (!youtubeId) return null
+
+  return (
+    <LiteYouTubeEmbed
+      id={youtubeId}
+      adNetwork={true}
+      params=''
+      playlist={false}
+      poster='hqdefault'
+      title='YouTube Embed'
+      noCookie={true}
+      wrapperClass={clsx(styles.YoutubeEmbedWrapper)}
+      activatedClass='youtube-activated'
+      playerClass={clsx(styles.YoutubeEmbedPlayer)}
+      iframeClass={styles.YoutubeEmbedIframe}
+      aspectHeight={9}
+      aspectWidth={16}
+    />
+  )
+}
+
+function GeneralEmbed({ src }: { src: string }) {
+  return (
+    <iframe
+      width='100%'
+      height='100%'
+      src={src}
+      frameBorder='0'
+      loading='lazy'
+      allow='autoplay; encrypted-media; picture-in-picture;'
+      allowFullScreen
+      title='Embedded'
+      style={{ position: 'absolute', top: 0, left: 0 }}
+    />
   )
 }
 
