32817e48ccbc6bf83af5282e87c96ccfbb244e4d
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2023-12-13 11:21:09 -0600

    Update polkaverseSpaces.ts

--- src/config/app/polkaverse/polkaverseSpaces.ts
+++ src/config/app/polkaverse/polkaverseSpaces.ts
@@ -5,16 +5,12 @@ export default [
   '1354',
   // The Kusamarian
   '4809',
-  // Substrate Newsletters
-  '10647',
+  // Filippo W3F
+  '10124',
   // Zeitgeist
   '4306',
   // Bifrost
   '1013',
-  // Ajuna Network
-  '6363',
-  // Gafi Network
-  '7248',
   // Astar Network
   '10802',
   // Astar Network dApp Staking updates
@@ -25,38 +21,18 @@ export default [
   '6283',
   // Polkadot Arena (Italian)
   '7218',
-  // Todo en cripto (Spanish)
-  '7272',
   // Web3 Chinese
   '10900',
-  // Polkadot Hungary
-  '11025',
   // Polkadot Romania
   '7171',
   // Dotsama Bulgaria
   '10652',
-  // Blinkin.dot
-  '5066',
-  // DotLeap
-  '1040',
-  // Pnin's "KSM/DOT Ecosystem: Thoughts, NFTs, Rumors, News and Events"
-  '5892',
   // CryptoZilla
   '11581',
   // Polkadot Insider
   '11157',
-  // Creator Staking
-  '11414',
-  // Alice und Bob
-  '4777',
   // Goku
   '6953',
-  // Dotcast
-  '10132',
-  // LV
-  '7366',
-  // Alpha Airdrop
-  '11566',
   // Astar Korea
   '11991',
 ]
