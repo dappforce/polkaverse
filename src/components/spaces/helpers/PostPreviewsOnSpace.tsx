0cdf1749b107351a63406ebb6cf6452ad6271303
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-12-20 00:16:24 +0700

    style: remove post count title in post page because of redundant data

--- src/components/spaces/helpers/PostPreviewsOnSpace.tsx
+++ src/components/spaces/helpers/PostPreviewsOnSpace.tsx
@@ -1,11 +1,10 @@
-import React, { useCallback } from 'react'
+import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 import { InnerLoadMoreFn } from 'src/components/lists'
 import { InfinitePageList } from 'src/components/lists/InfiniteList'
 import { PublicPostPreviewById } from 'src/components/posts/PublicPostPreview'
 import { useSubsocialApi } from 'src/components/substrate/SubstrateContext'
 import { getPageOfIds } from 'src/components/utils/getIds'
-import { Pluralize } from 'src/components/utils/Plularize'
 import { fetchPosts } from 'src/rtk/features/posts/postsSlice'
 import { DataSourceTypes, PostId, PostWithSomeDetails, SpaceData } from 'src/types'
 import { FollowerCanPostAlert } from '../permissions/FollowerCanPostAlert'
@@ -16,21 +15,6 @@ type Props = {
   postIds: PostId[]
   posts: PostWithSomeDetails[]
 }
-
-const PostsSectionTitle = React.memo((props: Props) => {
-  const { spaceData } = props
-  const { struct: space } = spaceData
-  const { postsCount } = space
-
-  return (
-    <div className='w-100 d-flex justify-content-between align-items-baseline'>
-      <span style={{ marginRight: '1rem' }}>
-        <Pluralize count={postsCount || 0} singularText='Post' />
-      </span>
-      {!!postsCount && <CreatePostButton space={space} title={'Write Post'} className='mb-2' />}
-    </div>
-  )
-})
 
 const InfiniteListOfPublicPosts = (props: Props) => {
   const { spaceData, posts, postIds } = props
@@ -54,7 +38,6 @@ const InfiniteListOfPublicPosts = (props: Props) => {
     () => (
       <InfinitePageList
         loadingLabel='Loading more posts...'
-        title={<PostsSectionTitle {...props} />}
         dataSource={initialPostIds}
         loadMore={loadMore}
         totalCount={postsCount || 0}
