93119aa93c760ceb81e4033553fb0eda4ec29b41
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-12-29 21:37:06 +0700

    fix: flickering when first render space page

--- src/components/spaces/ViewSpace.tsx
+++ src/components/spaces/ViewSpace.tsx
@@ -124,7 +124,7 @@ export const InnerViewSpace = (props: Props) => {
   const setChatConfig = useSetChatEntityConfig()
   const setChatOpen = useSetChatOpen()
 
-  const { isCreatorSpace } = useIsCreatorSpace(spaceData?.id)
+  const { isCreatorSpace, loading } = useIsCreatorSpace(spaceData?.id)
 
   // We do not return 404 page here, because this component could be used to render a space in list.
   if (!spaceData) return null
@@ -292,7 +292,7 @@ export const InnerViewSpace = (props: Props) => {
     )
   }
 
-  const showCreatorCards = isCreatorSpace && isMobile
+  const showCreatorCards = isCreatorSpace && isMobile && !loading
 
   return (
     <Section className='mt-3'>
