35a1b65aa2933776aff913aa026dc430c4accb1d
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-01-02 23:43:41 +0700

    feat: show my stake in creator info

--- src/components/creators/cards/CreatorInfoCard.tsx
+++ src/components/creators/cards/CreatorInfoCard.tsx
@@ -1,24 +1,29 @@
 import { SpaceData } from '@subsocial/api/types'
 import { Button } from 'antd'
 import clsx from 'clsx'
+import { BsBoxArrowUpRight } from 'react-icons/bs'
 import { useMyAddress } from 'src/components/auth/MyAccountsContext'
+import { FormatBalance } from 'src/components/common/balances'
 import { SpaceFollowersModal } from 'src/components/profiles/AccountsListModal'
 import { OfficialSpaceStatus, SpaceAvatar } from 'src/components/spaces/helpers'
 import CollapsibleParagraph from 'src/components/utils/CollapsibleParagraph/CollapsibleParagraph'
 import FollowSpaceButton from 'src/components/utils/FollowSpaceButton'
 import { Pluralize } from 'src/components/utils/Plularize'
 import Segment from 'src/components/utils/Segment'
+import { useIsCreatorSpace } from 'src/rtk/features/creators/creatorsListHooks'
 import { useFetchStakeData } from 'src/rtk/features/creators/stakesHooks'
 import { getSubIdCreatorsLink } from 'src/utils/links'
 import styles from './CreatorInfoCard.module.sass'
 
 export type CreatorInfoCardProps = {
   space: SpaceData
+  showStakeButton?: boolean
 }
 
-export default function CreatorInfoCard({ space }: CreatorInfoCardProps) {
-  const myAddress = useMyAddress()
-  const { data } = useFetchStakeData(myAddress ?? '', space.id)
+export default function CreatorInfoCard({ space, showStakeButton = true }: CreatorInfoCardProps) {
+  const myAddress = useMyAddress() ?? ''
+  const { isCreatorSpace } = useIsCreatorSpace(space.id)
+  const { data: stakeData } = useFetchStakeData(myAddress, space.id)
 
   return (
     <Segment className={clsx(styles.CreatorInfoCard)}>
@@ -42,15 +47,36 @@ export default function CreatorInfoCard({ space }: CreatorInfoCardProps) {
           />
         </div>
       </div>
-      <CollapsibleParagraph className='FontSmall mb-3' text={space.content?.about ?? ''} />
-      <div className='GapSmall d-flex flex-column'>
-        {data?.hasStaked && (
-          <Button target='_blank' type='primary' href={getSubIdCreatorsLink(space)}>
-            Stake
+      <CollapsibleParagraph className='FontSmall mb-2' text={space.content?.about ?? ''} />
+      {!stakeData?.hasStaked ? (
+        <div className='GapSmall d-flex flex-column mt-2'>
+          {isCreatorSpace && showStakeButton && (
+            <Button target='_blank' type='primary' href={getSubIdCreatorsLink(space)}>
+              Stake
+            </Button>
+          )}
+          <FollowSpaceButton space={space.struct} />
+        </div>
+      ) : (
+        <div className={clsx('d-flex flex-column GapNormal', styles.MyStake)}>
+          <div className='GapSmall d-flex align-items-center justify-content-between FontSmall'>
+            <span className='ColorMuted'>My Stake</span>
+            <span className='FontWeightMedium'>
+              <FormatBalance value={stakeData?.stakeAmount} isShort decimals={10} currency='SUB' />
+            </span>
+          </div>
+          <Button
+            className={clsx('d-flex align-items-center GapTiny justify-content-center pt-1')}
+            type='primary'
+            ghost
+            href={getSubIdCreatorsLink(space)}
+            target='_blank'
+          >
+            Manage my stake
+            <BsBoxArrowUpRight />
           </Button>
-        )}
-        <FollowSpaceButton space={space.struct} />
-      </div>
+        </div>
+      )}
     </Segment>
   )
 }
