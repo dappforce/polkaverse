e5769cf9771473c93ed7f6ee9dcb64e8281e12fa
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-01-12 21:35:03 +0700

    chore: update wording

--- src/components/main/HomePageFilters.tsx
+++ src/components/main/HomePageFilters.tsx
@@ -29,7 +29,7 @@ const commonFilterOption = [{ label: 'Latest', value: 'latest' }]
 //   : []
 
 export const postFilterOpt = [
-  { label: 'Active Staking', value: 'suggested' },
+  { label: 'Featured Posts', value: 'suggested' },
   { label: 'All Posts', value: 'latest' },
   // removed most liked and commented
   // ...offchainPostFilterOpt,
