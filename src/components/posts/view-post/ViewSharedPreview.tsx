188983f7d3690fb1a6b25f54d504eff2256cc57d
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-02-07 18:21:11 +0700

    feat: add follow button to shared post and adjust size of edit button

--- src/components/posts/view-post/ViewSharedPreview.tsx
+++ src/components/posts/view-post/ViewSharedPreview.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react'
+import { useIsMobileWidthOrDevice } from 'src/components/responsive'
+import FollowSpaceButton from 'src/components/utils/FollowSpaceButton'
 import { useSelectPost } from 'src/rtk/app/hooks'
 import { asSharedPostStruct } from 'src/types'
 import { InnerPreviewProps } from '.'
@@ -9,12 +11,24 @@ type ComponentType = FC<InnerPreviewProps>
 
 export const SharedPreview: ComponentType = props => {
   const { postDetails, space, withActions } = props
+  const isMobile = useIsMobileWidthOrDevice()
   const sharedPostId = asSharedPostStruct(postDetails.post.struct).originalPostId
   postDetails.ext = useSelectPost(sharedPostId)
 
   return (
     <>
       <div className='DfRow'>
+        {space && !isMobile && (
+          <FollowSpaceButton
+            className='mr-1 px-2'
+            style={{ height: '28px' }}
+            size='small'
+            type='default'
+            ghost={false}
+            space={space.struct}
+            withUnfollowButton={false}
+          />
+        )}
         <PostCreator withSpaceAvatar postDetails={postDetails} space={space} withSpaceName />
         <PostDropDownMenu space={space?.struct} post={postDetails.post} />
       </div>
