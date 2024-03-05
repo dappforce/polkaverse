72977d88b65012bdeacf58f2ec44462d4a62716d
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-05 22:15:50 +0700

    Add from url in sign in href

--- src/components/auth/AuthorizationPanel.tsx
+++ src/components/auth/AuthorizationPanel.tsx
@@ -1,8 +1,16 @@
 import { Button } from 'antd'
+import { getCurrentUrlOrigin } from 'src/utils/url'
+import urlJoin from 'src/utils/url-join'
 import { NotificationsBell, useNotifCounterContext } from '../activity/NotifCounter'
 import { NewPostButtonInTopMenu } from '../posts/NewPostButtonInTopMenu'
 import { MyAccountPopup } from '../profiles/address-views'
+import { isServerSide } from '../utils'
 import { useMyAddress } from './MyAccountsContext'
+
+function getCurrentPathanme() {
+  if (isServerSide()) return ''
+  return encodeURIComponent(window.location.href.replace(getCurrentUrlOrigin(), ''))
+}
 
 export const AuthorizationPanel = () => {
   const address = useMyAddress()
@@ -17,7 +25,7 @@ export const AuthorizationPanel = () => {
           <MyAccountPopup className='profileName' />
         </>
       ) : (
-        <a href='/c/widget/login'>
+        <a href={urlJoin('/c/widget/login', `?from=${getCurrentPathanme()}`)}>
           <Button type='default'>Sign in</Button>
         </a>
       )}
