6ee051b1c39654167b5efe3d056249d59c09619b
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-08 12:59:24 +0700

    Fix parse message

--- src/components/profile-selector/MyAccountMenu.tsx
+++ src/components/profile-selector/MyAccountMenu.tsx
@@ -67,7 +67,12 @@ const MyAccountDrawerContext = createContext<MyAccountSectionContextState>(initV
 export const useMyAccountDrawer = () => useContext(MyAccountDrawerContext)
 
 function parseMessage(data: string) {
-  const [origin, name, value] = data.split(':')
+  const match = data.match(/^([^:]+):([^:]+):(.+)$/)
+  if (!match) return null
+
+  const origin = match[1]
+  const name = match[2]
+  const value = match[3]
   if (origin !== 'grill') return null
   return { name: name ?? '', value: value ?? '' }
 }
