f24d1c5795d37da075939b36aab934058172c675
Sam <sam4uk.v1ad@gmail.com>
2024-03-13 14:07:32 +0200

    Add link for ask question button

--- src/components/energy/QuestionSection.tsx
+++ src/components/energy/QuestionSection.tsx
@@ -6,7 +6,7 @@ const QuestionSection = () => {
     <div className={styles.QuestionWrapper}>
       <div className={styles.QuestionTitle}>Still have questions?</div>
 
-      <Button type='primary' size='large' shape='round' ghost>
+      <Button type='primary' size='large' href='/c/ask' target='__blank' shape='round' ghost>
         Ask in chat
       </Button>
     </div>
