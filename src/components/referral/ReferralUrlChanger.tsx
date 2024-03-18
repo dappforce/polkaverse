3518e3ebd61908854567c037181d12913460cdda
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-03-19 01:08:52 +0700

    Fix custom link hydration error

--- src/components/referral/ReferralUrlChanger.tsx
+++ src/components/referral/ReferralUrlChanger.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import useIsMounted from 'src/hooks/useIsMounted'
 import { useSelectProfileSpace } from 'src/rtk/app/hooks'
 import { getCurrentSearchParams, getCurrentUrlWithoutQuery } from 'src/utils/url'
 import { useMyAddress } from '../auth/MyAccountsContext'
@@ -8,7 +9,9 @@ import { getReferralIdInUrl } from './utils'
 export function useReferralId() {
   const myAddress = useMyAddress()
   const profileSpace = useSelectProfileSpace(myAddress)
-  return profileSpace?.spaceId || getReferralIdInUrl()
+  const isMounted = useIsMounted()
+  // prevent hydration error
+  return profileSpace?.spaceId || (isMounted ? getReferralIdInUrl() : '')
 }
 
 export function ReferralUrlChanger() {
