c341edbfe6916a7ed91604555a00b0d550b747a4
Mell <oleh.melnechyk@gmail.com>
2024-03-26 16:38:13 +0200

    Add PINNED POST

--- src/components/posts/pinned-post.ts
+++ src/components/posts/pinned-post.ts
@@ -1,7 +1,7 @@
 import { getLastestPostIdsInSpace } from 'src/graphql/apis'
 import { GqlClient } from 'src/graphql/ApolloProvider'
 
-const PINNED_POST_IDS: string[] = ['']
+const PINNED_POST_IDS: string[] = ['106190']
 export const PINNED_POST_ID = PINNED_POST_IDS[Math.floor(Math.random() * PINNED_POST_IDS.length)]
 
 const COMMUNITY_SPACE_ID = '1244'
