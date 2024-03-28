4de6bb92400320928fb291533082fa52d29e20cc
Sam <sam4uk.v1ad@gmail.com>
2024-03-28 17:34:09 +0200

    Add more banners to carousel on the main page

--- src/components/creators/cards/SupportCreatorsCard.tsx
+++ src/components/creators/cards/SupportCreatorsCard.tsx
@@ -8,26 +8,39 @@ import { useFetchTotalStake } from 'src/rtk/features/creators/totalStakeHooks'
 import { getContentStakingLink } from 'src/utils/links'
 import styles from './SupportCreatorsCard.module.sass'
 
-const EarnInChatCard = () => {
+type BannerCardProps = {
+  title: React.ReactNode
+  subtitle: React.ReactNode
+  imagePath: string
+  learnMoreHref: string
+  buttonLabel: string
+  backgroundColor: string
+  titleColor: string
+}
+
+const BannerCard = ({
+  title,
+  subtitle,
+  imagePath,
+  learnMoreHref,
+  buttonLabel,
+  titleColor,
+}: BannerCardProps) => {
   const sendEvent = useSendEvent()
-  const learnMoreHref =
-    'https://grillapp.net/@olehmell/after-migrating-grill-to-the-mainnet-chat-messages-can-now-95874'
 
   return (
     <>
       <div className={styles.ContentInner}>
-        <DfImage
-          preview={false}
-          src='/images/creators/earn-sub-image.png'
-          className={clsx(styles.Image)}
-        />
-        <p className={clsx('FontWeightSemibold FontLarge mb-2', styles.Title)}>
-          Earn SUB rewards in chat!
-        </p>
-        <p className={clsx(styles.Subtitle, 'mb-3')}>
-          You can earn SUB rewards by chatting, offering helpful advice, and participating in
-          engaging discussions
-        </p>
+        <div className={styles.Info}>
+          <DfImage preview={false} src={imagePath} className={clsx(styles.Image)} />
+          <p
+            className={clsx('FontWeightSemibold mb-2', styles.Title)}
+            style={{ color: titleColor }}
+          >
+            {title}
+          </p>
+          {subtitle}
+        </div>
         <Button
           href={learnMoreHref}
           onClick={() =>
@@ -35,15 +48,14 @@ const EarnInChatCard = () => {
               value: learnMoreHref,
             })
           }
+          className={styles.LearnMoreButton}
           block
           shape='round'
           target='_blank'
         >
-          Learn more
+          {buttonLabel}
         </Button>
       </div>
-      <div className={styles.Gradient1} />
-      <div className={styles.Gradient2} />
     </>
   )
 }
@@ -59,23 +71,93 @@ export default function SupportCreatorsCard() {
   return isStaked ? <StakedCard /> : <NotStakedCard />
 }
 
+const EngageToEarnSubtitle = () => {
+  const diamond = (
+    <img
+      src='/images/creators/diamonds/diamond-list-item.svg'
+      className={clsx(styles.OutsideDiamondRight)}
+    />
+  )
+  return (
+    <>
+      <p className={clsx(styles.Subtitle, 'mb-3')}>Lock SUB and get rewards when you:</p>
+      <ul className={styles.List}>
+        <li className={clsx(styles.ListItem)}>
+          {diamond} <span>Like posts</span>
+        </li>
+        <li className={clsx(styles.ListItem)}>
+          {diamond} <span>Get engagement or likes on your content</span>
+        </li>
+      </ul>
+    </>
+  )
+}
+
 const StakedCard = () => {
   // EDIT ME: manage banners in carousel
   const data = [
     {
-      item: <EarnInChatCard />,
+      title: 'Engage-To-Earn',
+      subtitle: <EngageToEarnSubtitle />,
+
+      imagePath: '/images/creators/engage-to-earn.png',
+      learnMoreHref: getContentStakingLink(),
+      buttonLabel: 'Start Earning',
+      backgroundColor: '#DFEFFF',
+      titleColor: '#4972E9',
     },
-    // {
-    //   item: <EarnInChatCard />,
-    // },
+    {
+      title: 'Chat Monetization',
+      subtitle: (
+        <p className={clsx(styles.Subtitle, 'mb-3')}>
+          Join conversations, share your thoughts, and connect with others to start earning every
+          time your messages receive likes.
+        </p>
+      ),
+      imagePath: '/images/creators/chat-monetization.png',
+      learnMoreHref: 'https://grillapp.net/c/hot-chats',
+      buttonLabel: 'Explore Chats',
+      backgroundColor: '#DFFFDF',
+      titleColor: '#30AD30',
+    },
+    {
+      title: 'Establish Your Brand',
+      subtitle: (
+        <p className={clsx(styles.Subtitle, 'mb-3')}>
+          Secure your username and create your brand identity on Grill to stand out, be recognized,
+          and start growing your community.
+        </p>
+      ),
+      imagePath: '/images/creators/establish-your-brand.png',
+      learnMoreHref: 'https://grillapp.net/dd',
+      buttonLabel: 'Register Username',
+      backgroundColor: '#ECF1FF',
+      titleColor: '#0F172A',
+    },
+    {
+      title: 'Seamless Access',
+      subtitle: (
+        <p className={clsx(styles.Subtitle, 'mb-3')}>
+          Save your Grill key or QR code for hassle-free login access to Grill across all of your
+          devices.
+        </p>
+      ),
+      imagePath: '/images/creators/seamless-access.png',
+      learnMoreHref: 'https://grillapp.net/@olehmell/lets-use-login-with-grill-key-100229',
+      buttonLabel: 'Learn more',
+      backgroundColor: '#C0FEF3',
+      titleColor: '#1A3D7A',
+    },
   ]
 
   return (
     <Carousel autoplay autoplaySpeed={5000} className={styles.CarouselWrapper} pauseOnHover dots>
       {data.map((item, index) => (
-        <div className={styles.ItemContent} key={index}>
-          {item.item}
-          {data.length > 1 && <div className={styles.DotsSection}></div>}
+        <div key={index}>
+          <div className={styles.ItemContent} style={{ backgroundColor: item.backgroundColor }}>
+            <BannerCard {...item} />
+            {data.length > 1 && <div className={styles.DotsSection}></div>}
+          </div>
         </div>
       ))}
     </Carousel>
