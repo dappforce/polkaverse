629a24d29652a3b8a1cc90e2abdcc3cf9ba5291b
Mell <oleh.melnechyk@gmail.com>
2024-04-11 11:34:37 +0300

    Add logs for debugging energy problem

--- src/components/substrate/SubstrateTxButton.tsx
+++ src/components/substrate/SubstrateTxButton.tsx
@@ -112,7 +112,9 @@ function TxButton({
   const { api: subsocialApi } = useSubstrate()
   const openOnBoardingModal = useOpenCloseOnBoardingModal()
   const [isSending, , setIsSending] = useToggle(false)
+  const energy = useMyAccount(state => state.energy)
   const hasEnoughEnergy = useMyAccount(state => (state.energy ?? 0) >= ESTIMATED_ENERGY_FOR_ONE_TX)
+  console.log(hasEnoughEnergy, energy, ESTIMATED_ENERGY_FOR_ONE_TX)
   const waitHasEnergy = useWaitHasEnergy()
   const [isOpenBlockedModal, setIsOpenBlockedModal] = useState(false)
 
